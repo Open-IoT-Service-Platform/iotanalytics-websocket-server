db81c197b2c9c872e3ab355b1cfce11773124e36
Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>
2019-05-22 04:35:42 +0200

    Remove postgres pre-defined function dependency
    Signed-off-by: Oguzcan Kirmemis <oguzcan.kirmemis@gmail.com>

--- iot-entities/connectionBindings.js
+++ iot-entities/connectionBindings.js
@@ -15,28 +15,101 @@
  */
 'use strict';
 
-var sequelize = require('../iot-entities').sequelize;
+var sequelize = require('../iot-entities').sequelize,
+    DataTypes = require('sequelize'),
+    uuid = require('uuid/v4');
 
-var WEBSOCKET = 'ws',
-    QUERY = 'SELECT * FROM dashboard.update_device_connection (:deviceId, :server, :connectingStatus, :type)';
+var connectionBindings = sequelize.define('connectionBindings', {
+    id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        defaultValue: DataTypes.UUIDV4
+    },
+    deviceId: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        unique: 'connectionBindings_deviceId_type_unique'
+    },
+    lastConnectedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    server: {
+        type: DataTypes.STRING(20),
+        allowNull: false
+    },
+    connectingStatus: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    },
+    type: {
+        //Ordering in enum depends on creation date of type, so enum types should be listed in alfabetical order
+        type: DataTypes.ENUM('mqtt', 'ws'),
+        allowNull: false,
+        unique: 'connectionBindings_deviceId_type_unique'
+    }
+},
+{
+    createdAt: 'created',
+    updatedAt: 'updated',
+    indexes: [
+        {
+            name: 'connectionBindings_deviceId_index',
+            method: 'BTREE',
+            fields: ['deviceId']
+        },
+        {
+            name: 'connectionBindings_type_index',
+            method: 'BTREE',
+            fields: ['type']
+        }
+    ],
+    schema: 'dashboard'
+});
+
+var WEBSOCKET = 'ws';
 
 exports.update = function (deviceId, server, connectingStatus, resultCallback) {
-    var replacements = {
-        deviceId: deviceId,
+    var record = {
         server: server,
-        connectingStatus: connectingStatus,
-        type: WEBSOCKET
+        connectingStatus: connectingStatus
     };
 
-    return sequelize.query(QUERY, {replacements: replacements})
-        .then(function (result) {
-            if (!result || !result[0][0]) {
+    var filters = {
+        where: {
+            deviceId: deviceId,
+            type: WEBSOCKET
+        },
+        fields: ['server', 'connectingStatus'],
+        returning: true
+    };
+
+    if (connectingStatus === true) {
+        record.lastConnectedAt = new Date().getTime();
+        filters.fields.push('lastConnectedAt');
+    }
+
+    return connectionBindings.update(record, filters)
+        .then(result => {
+            if (!result) {
                 throw new Error('Unable to update record in db');
+            } else if (result[0] !== 1) {
+                record.deviceId = deviceId;
+                record.type = WEBSOCKET;
+                record.id = uuid();
+                record.lastConnectedAt = new Date().getTime();
+                return connectionBindings.create(record);
             } else {
-                resultCallback(null, result);
+                return new Promise(resolve => {
+                    resolve(result.length < 2 ? result[0] : result[1]);
+                });
             }
         })
-        .catch(function (err) {
+        .then(result => {
+            resultCallback(null, result);
+        })
+        .catch(err => {
             resultCallback(err);
         });
 };
